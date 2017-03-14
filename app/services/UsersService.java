509b88c09aa83f78bebfbd8ee99572c18486aad0
Jerome Samson <jerome.samson@edataconsulting.de>
2017-03-14 16:20:37 +0000

    Refactored UsersService and removed akka-http

--- app/services/UsersService.java
+++ app/services/UsersService.java
@@ -1,72 +1,20 @@
 package services;
 
-
-import daos.UserResultHandler;
 import daos.UsersDao;
 import models.pojos.TmUser;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 
 import javax.inject.Inject;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 
 public class UsersService {
 
-    @Inject
-    SqlSessionFactory sqlSessionFactory;
     @Inject
     private UsersDao usersDao;
 
     public List<TmUser> getUsers() {
 
         return usersDao.getUsers();
+
     }
 
-    public List<HashMap<String, String>> getUsersMap() {
-
-        return usersDao.getUsersMap();
-    }
-
-
-    public File getUsersFromFile() {
-
-        FileOutputStream fop = null;
-        File file;
-
-        try {
-
-            file = new File("users");
-            fop = new FileOutputStream(file);
-
-            file.createNewFile();
-
-            UserResultHandler userResultHandler = new UserResultHandler(fop);
-
-            SqlSession sqlSession = sqlSessionFactory.openSession();
-            sqlSession.select("getUsers", userResultHandler);
-
-
-            sqlSession.close();
-
-
-            fop.flush();
-            fop.close();
-
-            return file;
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        return null;
-    }
 }
--- build.sbt
+++ build.sbt
@@ -17,7 +17,5 @@ libraryDependencies ++= Seq(
 
   "com.google.inject.extensions"        %     "guice-multibindings"   %     "4.0",
 
-  "org.postgresql"                      %     "postgresql"            %     "9.4-1206-jdbc42",
-
-  "com.typesafe.akka"                   %%    "akka-http"             %     "10.0.4"
+  "org.postgresql"                      %     "postgresql"            %     "9.4-1206-jdbc42"
 )
